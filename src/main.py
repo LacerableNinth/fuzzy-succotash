97df9cab6731094fc1b9b67a7af654dcac18c8a5
ninth <Marcel Ehret - Arch>
2026-02-24 18:52:24 +0100

    CH5-L2

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import os, shutil
 from copystatic import copy_files_recursive
+from gencontent import generate_page
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 def main():
     print("Deleting public directory...")
@@ -12,5 +15,11 @@ def main():
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
+    print("Generating page...")
+    generate_page(
+        os.path.join(dir_path_content, "index.md"),
+        template_path,
+        os.path.join(dir_path_public, "index.html"),
+    )
 
 main()
